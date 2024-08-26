5820e63bf754a046e562f650d359069a1365df74
Jordan Lagan <jordanlagan000@gmail.com>
2024-08-25 20:52:33 -0400

    Contact form

---
+++ _contact_form_app/app.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'mail'
+
+configure do
+  set :port, 4567
+
+  Mail.defaults do
+    delivery_method :smtp, {
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: 'your-email@gmail.com',
+      password: 'your-email-password',
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+  end
+end
+
+post '/submit_form' do
+  name = params[:name]
+  email = params[:email]
+  message = params[:message]
+
+  mail = Mail.new do
+    from    email
+    to      'your-email@gmail.com'
+    subject 'New Contact Form Submission'
+    body    "Name: #{name}\nEmail: #{email}\nMessage: #{message}"
+  end
+
+  if mail.deliver
+    "Thank you for your message. We will get back to you soon."
+  else
+    "There was an error sending your message. Please try again later."
+  end
+end
+
+get '/thank_you' do
+  "Thank you for your message. We will get back to you soon."
+end
+
