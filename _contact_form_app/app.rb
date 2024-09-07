29b120b56907d1489b3aa7d4483c470d5d2537ba
Jordan Lagan <jordanlagan000@gmail.com>
2024-09-07 11:22:30 -0400

    Switched email to SES

--- _contact_form_app/app.rb
+++ _contact_form_app/app.rb
@@ -1,19 +1,14 @@
 require 'sinatra'
-require 'mail'
+require 'aws-sdk-ses'
 
 configure do
   set :port, 4567
 
-  Mail.defaults do
-    delivery_method :smtp, {
-      address: "smtp.gmail.com",
-      port: 587,
-      user_name: ENV["EMAIL_USER"],
-      password: ENV["EMAIL_PASSWORD"],
-      authentication: 'plain',
-      enable_starttls_auto: true
-    }
-  end
+  Aws.config.update({
+    region: 'us-east-1', # e.g., 'us-east-1'
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+  })
+  set :ses, Aws::SES::Client.new
 end
 
 post '/submit_form' do
@@ -21,21 +16,22 @@ post '/submit_form' do
   email = params[:email]
   message = params[:message]
 
-  mail = Mail.new do
-    from    email
-    to      ENV["EMAIL_USER"]
-    subject 'New Contact Form Submission'
-    body    "Name: #{name}\nEmail: #{email}\nMessage: #{message}"
-  end
-
-  if mail.deliver
+  ses = settings.ses
+  begin
+    ses.send_email({
+      destination: { to_addresses: ['jordanlagan000@gmail.com'] },
+      message: {
+        body: { text: { data: "Name: #{name}\nEmail: #{email}\nMessage: #{message}" }},
+        subject: { data: 'New Contact Form Submission' }
+      },
+      source: 'jordanlagan000@gmail.com'
+    })
     "Thank you for your message. We will get back to you soon."
-  else
-    "There was an error sending your message. Please try again later."
+  rescue Aws::SES::Errors::ServiceError => error
+    "There was an error sending your message: #{error.message}"
   end
 end
 
 get '/thank_you' do
   "Thank you for your message. We will get back to you soon."
 end
-
