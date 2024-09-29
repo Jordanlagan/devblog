b3c7e55a3834ca53a2752ae6f33813f43fd1a0b0
Jordan Lagan <jordanlagan000@gmail.com>
2024-09-29 10:30:40 -0400

    Added honeypot to form

--- _contact_form_app/app.rb
+++ _contact_form_app/app.rb
@@ -21,21 +21,43 @@ post '/submit_form' do
   name = params[:name]
   email = params[:email]
   message = params[:message]
+  honeypot = params[:honeypot]
+
+  # Check honeypot field
+  if honeypot && !honeypot.empty?
+    halt 400, "Error: Form submission detected as spam."
+  end
 
   mail = Mail.new do
-    from    'formsubmissions@jordanlagan.com'   # This email must be verified in SES
+    from    'jordanlagan000@gmail.com'   # This email must be verified in SES
     to      'jordanlagan000@gmail.com'
     subject 'New Contact Form Submission'
     body    "Name: #{name}\nEmail: #{email}\nMessage: #{message}"
   end
 
   if mail.deliver
-    "Thank you for your message. We will get back to you soon."
+    redirect '/thank_you'
   else
-    "There was an error sending your message. Please try again later."
+    redirect '/error'
   end
 end
 
 get '/thank_you' do
-  "Thank you for your message. We will get back to you soon."
+  <<-HTML
+  <div style="text-align: center; padding: 20px;">
+    <h1>Thank You!</h1>
+    <p>Your message has been sent successfully. I'll get back to you shortly.</p>
+    <a href="/" style="color: blue; text-decoration: underline;">Back to homepage</a>
+  </div>
+  HTML
+end
+
+get '/error' do
+  <<-HTML
+  <div style="text-align: center; padding: 20px;">
+    <h1>Oops!</h1>
+    <p>Something went wrong. Please try again later.</p>
+    <a href="/contact" style="color: blue; text-decoration: underline;">Go back to the contact form</a>
+  </div>
+  HTML
 end
