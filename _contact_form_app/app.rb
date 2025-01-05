cb405a51f96173d8b24d1c38b6c498ec728a730c
Jordan Lagan <jordanlagan000@gmail.com>
2025-01-05 15:11:17 -0500

    Fixed captcha

--- _contact_form_app/app.rb
+++ _contact_form_app/app.rb
@@ -1,8 +1,5 @@
 require 'sinatra'
 require 'mail'
-require 'securerandom'
-
-enable :sessions  # Enable session support
 
 configure do
   set :port, 4567
@@ -10,28 +7,14 @@ configure do
   # Configure Mail gem to use AWS SES SMTP settings
   Mail.defaults do
     delivery_method :smtp, {
-      address: 'email-smtp.us-east-1.amazonaws.com',
-      port: 587,
-      user_name: ENV['SES_SMTP_USERNAME'],
-      password: ENV['SES_SMTP_PASSWORD'],
-      authentication: :login,
-      enable_starttls_auto: true
+      address: 'email-smtp.us-east-1.amazonaws.com', # e.g., 'email-smtp.us-east-1.amazonaws.com'
+      port: 587,                                       # Can also use 465 for SSL
+      user_name: ENV['SES_SMTP_USERNAME'],             # Use the SMTP username from SES
+      password: ENV['SES_SMTP_PASSWORD'],              # Use the SMTP password from SES
+      authentication: :login,                          # Use :login or :plain based on SES SMTP
+      enable_starttls_auto: true                       # Use TLS
     }
   end
-end
-
-before '/submit_form' do
-  # Generate a simple math problem
-  if session[:captcha_answer].nil?
-    num1 = rand(1..10)
-    num2 = rand(1..10)
-    session[:captcha_answer] = num1 + num2
-    session[:captcha_question] = "#{num1} + #{num2}"
-  end
-end
-
-get '/contact' do
-  erb :contact, locals: { captcha_question: session[:captcha_question] }
 end
 
 post '/submit_form' do
@@ -39,7 +22,8 @@ post '/submit_form' do
   email = params[:email]
   message = params[:message]
   honeypot = params[:honeypot]
-  captcha = params[:captcha]
+  captcha = params[:captcha].to_i
+  captcha_answer = params[:captcha_answer].to_i
 
   # Check honeypot field
   if honeypot && !honeypot.empty?
@@ -47,12 +31,9 @@ post '/submit_form' do
   end
 
   # Validate CAPTCHA
-  if captcha.to_i != session[:captcha_answer].to_i
+  if captcha != captcha_answer
     halt 400, "Error: CAPTCHA validation failed."
   end
-
-  # Clear the CAPTCHA from session after use
-  session[:captcha_answer] = nil
 
   mail = Mail.new do
     from    'jordanlagan000@gmail.com'
